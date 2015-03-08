6ecb9bdc9e9d7775a701375581f5434de09a9fd7
AngelM701 <thecrazymariachi@gmail.com>
2015-03-07 18:43:14 -0600

    register.rb
    Register file for apps/website

---
+++ register.rb
@@ -0,0 +1,13 @@
+print "Please enter your first name" #asks for their name
+first_name = gets.chomp #this will get saved to tell the person their info
+first_name.capitalize! #this capitalizes the first letter of the persons name
+
+print "please enter your last name" #asks for their last name
+last_name = gets.chomp #this will get saved to tell the person their info
+last_name.capitalize! #this capitalizes the first letter of the persons last name
+
+print "please enter your age" #asks for their age
+age = get.chomp #this will get saved to tell the person their info
+age.lowercase! #this makes the number lower case which is unnecessary
+
+puts "you have been registered as #{first_name} #{last_name}, #{13} years old" #it tells the person what they've enter"
