ac8ebc474bb5e53eb4a8f8186ef4ef693e59cfc0
AngelM701 <thecrazymariachi@gmail.com>
2015-03-07 20:13:04 -0600

    Update register.rb

--- register.rb
+++ register.rb
@@ -10,4 +10,4 @@ print "please enter your age" #asks for their age
 age = get.chomp #this will get saved to tell the person their info
 age.lowercase! #this makes the number lower case which is unnecessary
 
-puts "you have been registered as #{first_name} #{last_name}, #{13} years old" #it tells the person what they've enter"
+puts "you have been registered as #{first_name} #{last_name}, #{age} years old" #it tells the person what they've enter"
