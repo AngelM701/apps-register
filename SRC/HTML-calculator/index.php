2e3efabc6e0dc30642728688de921d437cfda649
Angel Maya <thecrazymariachi@gmail.com>
2015-03-07 21:20:23 -0600

    Create index.php
    You must compile [index.php with index.html] for it to work!

---
+++ SRC/HTML-calculator/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
+$operation = $_POST["operation"];
+
+if ($operarion = "add")
+{
+ $answer_add = $num1 + $num2;
+ echo $num1." + ".$num2." = ";
+ echo $answer_add;
+ }
+ 
+if ($operation = "subtract")
+{
+ $answer_subtract = $num1 - $num2;
+ echo $num1." - ".$num2." = ";
+ echo $answer_subtract;
+ }
+ 
+if ($operation = "multiply")
+{
+ $answer_multiply = $num1 * $num2;
+ echo $num1." * ".$num2." = ";
+ echo $answer_multiply;
+ }
+ 
+ if ($operation = "divide")
+ {
+  $answer_divide = $num1 / $num2
+  echo $num1." / ".$num2." = ";
+  echo $answer_divide;
+}
